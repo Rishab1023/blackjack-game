3381585d1027add19753bc7b01bd1228e6a7806b
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-08-30 20:48:13 +0530

    Add initial HTML and JavaScript files for blackjack game

---
+++ index.js
@@ -0,0 +1,8 @@
+//create two variables called firstcard and secondcard 
+//set their values to random number btw 2 and 11
+
+let firstcard = 2;
+let secondcard = 11;
+
+//create a variable, sum, and set it to the sum of the two cards
+
