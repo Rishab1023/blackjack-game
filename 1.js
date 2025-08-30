a225c1c8872df8c87de22de668c9c59d4d359ca1
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-08-30 22:41:37 +0530

    Implement age check for nightclub entry with console logging

---
+++ 1.js
@@ -0,0 +1,15 @@
+// check if person is old enough to enter the nightclub (21)
+// log a suitable message to the console in both cases
+
+let age = 22
+
+// if less than 21 -> "you cannot enter the club! "
+// else            -> "Welcome"
+
+if (age < 21) {
+  console.log("you cannot enter the club!");
+  
+} else {
+  console.log("Welcome");
+  
+}
