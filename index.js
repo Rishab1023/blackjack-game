89428fecdbd3fa64c47a4b35c4e9639a733ccc94
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-08-30 21:00:33 +0530

    Update card values and enhance game logic messages in blackjack

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let firstcard = 2;
+let firstcard = 11;
 let secondcard = 11;
 let sum = firstcard + secondcard
 
 console.log(sum);
 
 if (sum < 21) {
-  console.log();
+  console.log("Do you want to draw another card?");
+  
+} else if (sum === 21) {
+  console.log("Congrats, You have won");
+  
+} else if (sum > 21) {
+  console.log("You have lost");
   
 }
 
