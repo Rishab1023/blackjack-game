1c2182b1447f9ab9cfd22a4a8bc611194a317234
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-08-31 14:24:04 +0530

    Refactor message handling in blackjack game to consolidate console logging

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ let sum = firstcard + secondcard
 let hasBlackjack = false
 let isAlive = true
 // console.log(sum);
+let message = "";
 
 if (sum < 21) {
-  console.log("Do you want to draw another card?");
-  
+  message = "Do you want to draw another card?"
 } else if (sum === 21) {
-  console.log("Congrats, You have won");
+  message = "Congrats, You have won"
   hasBlackjack = true
 } else if (sum > 21) {
-  console.log("You have lost");
+  message = "You have lost"
   isAlive = false
 }
 
 // console.log(hasBlackjack);
-console.log(isAlive);
+// console.log(isAlive);
+
+console.log(message);
+
 
