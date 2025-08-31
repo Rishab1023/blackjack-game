b10e6a7a7e1a236702bc5caaae34b0e6fec0f353
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-08-31 13:54:31 +0530

    Refactor eligibility checks and logging for birthday card and blackjack game

--- 1.js
+++ 1.js
@@ -22,13 +22,22 @@ let age = 101
 // else if exactly 100 -> "Here is your birthday card from the King!"
 // else                -> "Not elegible, you have already gotten one"
 
-if (age < 100) {
-  console.log("Not elegible");
+// if (age < 100) {
+//   console.log("Not elegible");
   
-} else if (age === 100) {
-  console.log("Here is your birthday card from the King!");
+// } else if (age === 100) {
+//   console.log("Here is your birthday card from the King!");
   
-} else {
-  console.log("Not elegible, you have already gotten one");
+// } else {
+//   console.log("Not elegible, you have already gotten one");
   
-}
+// }
+
+
+console.log(4 === 3)  // false
+console.log(5 > 2)    // true
+console.log(12 > 12)  // false
+console.log(3 < 0)    // false
+console.log(3 >= 3)   // true
+console.log(11 <= 11) // true
+console.log(3 <= 2)   // false
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 let firstcard = 11;
 let secondcard = 11;
 let sum = firstcard + secondcard
-
-console.log(sum);
+let hasBlackjack = false
+let isAlive = true
+// console.log(sum);
 
 if (sum < 21) {
   console.log("Do you want to draw another card?");
   
 } else if (sum === 21) {
   console.log("Congrats, You have won");
-  
+  hasBlackjack = true
 } else if (sum > 21) {
   console.log("You have lost");
-  
+  isAlive = false
 }
+
+// console.log(hasBlackjack);
+console.log(isAlive);
 
