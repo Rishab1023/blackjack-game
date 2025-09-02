6d12ece2d30d56ee5e87c199e84118401abb6399
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-09-02 19:55:09 +0530

    Update UI and logic for BlackJack game
    Standardized element IDs in HTML and JS for consistency, added a 'New card' button, and improved game logic to update UI elements dynamically. Also enhanced button styling in CSS.

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
+// Game elements
 let firstcard = 10;
 let secondcard = 11;
-let sum = firstcard + secondcard
-let hasBlackjack = false
-let isAlive = true
-// console.log(sum);
+let sum = firstcard + secondcard;
+let hasBlackjack = false;
+let isAlive = true;
 let message = "";
-// console.log(hasBlackjack);
-// console.log(isAlive);
 
+// Game logic
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
+// start button
 function startGame() {
   console.log(firstcard, "" ,secondcard);
   if (sum < 21) {
-  message = "Do you want to draw another card?"
+  message = "Do you want to draw another card?";
 } else if (sum === 21) {
-  message = "Congrats, You have won"
-  hasBlackjack = true
+  message = "Congrats, You have won";
+  hasBlackjack = true;
 } else if (sum > 21) {
-  message = "You have lost"
-  isAlive = false
+  message = "You have lost";
+  isAlive = false;
 }
 console.log(message);
+messageEl.innerText = message;
+sumEl.innerText += sum;
+cardsEl.textContent = "Cards: " + firstcard + " " + secondcard;
+}
+
+function newcard() {
+  console.log("Drawing a new card from the deck");
+  
 }
