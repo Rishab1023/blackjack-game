bc5f1c685eda8bad498115d548dc65121dab125c
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-09-02 18:51:54 +0530

    Update BlackJack UI and game logic
    Refactored the game start logic into a startGame function and updated the button label in index.html. Improved styling in style.css, including a new background image (table.png), color adjustments, and button enhancements. Added a new table.png asset for the background.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-let firstcard = 11;
+let firstcard = 10;
 let secondcard = 11;
 let sum = firstcard + secondcard
 let hasBlackjack = false
 let isAlive = true
 // console.log(sum);
 let message = "";
+// console.log(hasBlackjack);
+// console.log(isAlive);
 
-if (sum < 21) {
+function startGame() {
+  console.log(firstcard, "" ,secondcard);
+  if (sum < 21) {
   message = "Do you want to draw another card?"
 } else if (sum === 21) {
   message = "Congrats, You have won"
@@ -15,16 +19,5 @@ if (sum < 21) {
   message = "You have lost"
   isAlive = false
 }
-
-// console.log(hasBlackjack);
-// console.log(isAlive);
-
 console.log(message);
-
-let blackjackEl = document.getElementById("BlackJackEl");
-let cardsEl = document.getElementById("cardsEl");
-let sumEl = document.getElementById("sumEl");
-
-// function Play() {
-//   cardsEl.innerText = 
-// }
+}
