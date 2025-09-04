76faab2c9a65ecefcc54c1fb6798b437c0dcf080
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-09-04 20:32:17 +0530

    Add array examples and push method usage
    Expanded the file with examples of arrays, including personal profile and experience lists, and demonstrated the use of the push method to add items to arrays. Also added a messages array and logged the result after pushing a new message.

--- 1.js
+++ 1.js
@@ -1,43 +1,97 @@
-// // check if person is old enough to enter the nightclub (21)
-// // log a suitable message to the console in both cases
+// // // check if person is old enough to enter the nightclub (21)
+// // // log a suitable message to the console in both cases
 
-// let age = 20
+// // let age = 20
 
-// // if less than 21 -> "you cannot enter the club! "
-// // else            -> "Welcome"
+// // // if less than 21 -> "you cannot enter the club! "
+// // // else            -> "Welcome"
 
-// if (age < 21) {
-//   console.log("you cannot enter the club!");
+// // if (age < 21) {
+// //   console.log("you cannot enter the club!");
   
-// } else {
-//   console.log("Welcome");
+// // } else {
+// //   console.log("Welcome");
   
-// }
+// // }
 
-// Check if the person is elegible for a birthday card from the King! (100)
+// // Check if the person is elegible for a birthday card from the King! (100)
 
-let age = 101
+// let age = 101
 
-// if less than 100    -> "Not elegible"
-// else if exactly 100 -> "Here is your birthday card from the King!"
-// else                -> "Not elegible, you have already gotten one"
+// // if less than 100    -> "Not elegible"
+// // else if exactly 100 -> "Here is your birthday card from the King!"
+// // else                -> "Not elegible, you have already gotten one"
 
-// if (age < 100) {
-//   console.log("Not elegible");
+// // if (age < 100) {
+// //   console.log("Not elegible");
   
-// } else if (age === 100) {
-//   console.log("Here is your birthday card from the King!");
+// // } else if (age === 100) {
+// //   console.log("Here is your birthday card from the King!");
   
-// } else {
-//   console.log("Not elegible, you have already gotten one");
+// // } else {
+// //   console.log("Not elegible, you have already gotten one");
   
-// }
+// // }
 
 
-console.log(4 === 3)  // false
-console.log(5 > 2)    // true
-console.log(12 > 12)  // false
-console.log(3 < 0)    // false
-console.log(3 >= 3)   // true
-console.log(11 <= 11) // true
-console.log(3 <= 2)   // false
+// console.log(4 === 3)  // false
+// console.log(5 > 2)    // true
+// console.log(12 > 12)  // false
+// console.log(3 < 0)    // false
+// console.log(3 >= 3)   // true
+// console.log(11 <= 11) // true
+// console.log(3 <= 2)   // false
+
+// //Arrays
+
+// //create an array that lists your i.e. education, experience, lisences, skills or similar
+// // the items should be strings
+
+// let Myprofile = [
+//   "I am currently studying computer science engineering, i am a first year student",
+//   "I am a begineer javascript developer",
+//   "I currently dont have any lisences",
+//   "I know basics of web development"
+// ]
+
+// let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
+
+// // Make the following appear in the console:
+// // Frontend developer at Xeneta
+// // People counter for Norstat
+// // CEO at Scrimba
+
+// console.log(experience[1]);
+// console.log(experience[2]);
+// console.log(experience[0]);
+
+// Array - ordered list of items - coposite / complex data type
+// let personal_info = [
+//   "Rishab",17,true
+// ]
+// Create an array that describes yourself. Use the three primitive data types you've learned
+// It should contain your name (string), your age (number), and whether you like pizza (boolean)
+
+let cards = [1,4]
+
+cards.push(6)
+
+// console.log(cards);
+
+// here cards are objects, push is method
+
+
+// Push the newMessage to the messages array, and then log out the array
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMessage = "Same here!"
+
+messages.push(newMessage)
+
+console.log(messages);
+
