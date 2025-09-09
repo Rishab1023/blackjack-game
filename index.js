7d50b1843e36018a0f830c5d01f4d6fdbca0badd
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-09-09 22:50:33 +0530

    Refactor random number generation and add random card selection logic

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let sum = firstcard + secondcard;
 let hasBlackjack = false;
 let isAlive = true;
 let message = "";
-let random_card = Math.random();
-console.log(random_card);
+let random_number = Math.random();
+console.log(random_number);
 // Game logic
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
@@ -39,4 +39,37 @@ function newcard() {
   sum = sum + thirdcard;
   cards.push(thirdcard);
   renderGame();
+}
+
+function random() {
+  if(random<=0.1){
+  thirdcard = 1
+  }
+  if(random<=0.2 and random>0.1){
+  thirdcard = 2 
+  }
+  if(random<=0.3 and random>0.2){
+  thirdcard = 3 
+  }
+  if(random<=0.4 and random>0.3){
+  thirdcard = 4 
+  }
+  if(random<=0.5 and random>0.4){
+  thirdcard = 5 
+  }
+  if(random<=0.6 and random>0.5){
+  thirdcard = 6 
+  }
+  if(random<=0.7 and random>0.6){
+  thirdcard = 7 
+  }
+  if(random<=0.8 and random>0.7){
+  thirdcard = 8
+  }
+  if(random<=0.9 and random>0.8){
+  thirdcard = 9 
+  }
+  else{
+  thirdcard = 10
+  }
 }
