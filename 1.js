1d06c90fc4c5ba0e5ae417759f2825272f9177a4
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-09-13 19:57:25 +0530

    Add message logging and loop examples; remove tempCodeRunnerFile.js

--- 1.js
+++ 1.js
@@ -102,10 +102,34 @@
 
 // example
 
-//   Start          finish      step size
-// for (let count = 0; count < 21; count++) {
+  // Start          finish      step size
+// for (let count = 10; count < 21; count++) {
 //   console.log(count);
 // }
-let random = Math.random();
+// let random = Math.random();
 
-console.log(random);
+// console.log(random);
+
+
+// create a loop that counts from 10 to 100 in steps of 10
+// console log the numbers
+
+
+// for (let count = 10; count < 101; count += 10) {
+//   console.log(count);
+// }
+
+let messages = [
+    "Hey, how's it going?",        
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately.",
+    "Same here!",
+    "Great to hear",
+    "🙌",
+    "fdfd"
+]
+
+for(let i = 0; i < messages.length; i += 1) {
+  console.log(messages);
+  
+}
